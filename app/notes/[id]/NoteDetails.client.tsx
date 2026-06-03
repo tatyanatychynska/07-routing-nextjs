ade1e35c92f3e84970a4a6645b382b86636e5ccc
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-06-03 04:51:24 +0200

    Updated useQuery param

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -15,6 +15,7 @@ export default function NoteDetailsClient() {
   } = useQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
   });
 
   if (isLoading) return <p>Loading, please wait...</p>;
